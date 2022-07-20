91c07e7915f0a95009a9fb64cc78b420f88d9090
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-07-20 13:43:02 +0530

    fixed some issues

--- src/component/Task/Task.jsx
+++ src/component/Task/Task.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import styles from "./task.module.css";
-// import Counter from "../Counter/Counter"
 import AddTask from "../AddTask/AddTask"
-// import CountdownTimer from '../Timer/CountdownTimer'
 import styled from "styled-components";
 import { FaTrash } from "react-icons/fa";
 
@@ -52,10 +50,6 @@ const Task = () => {
  };
 
 
- const THREE_DAYS_IN_MS = 1 * 1 * 15 * 15 * 1000;
- const NOW_IN_MS = new Date().getTime();
- const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
-
 
   return (
     <Container>
@@ -65,8 +59,6 @@ const Task = () => {
           <Li data-testid="task" key={e.id} className={styles.task} >
             <input type="checkbox" style={{margin:"15px"}} data-testid="task-checkbox" />
             <div>{e}</div>
-            {/* <Counter /> */}
-            {/* <CountdownTimer targetDate={dateTimeAfterThreeDays}  /> */}
             <button data-testid="task-remove-button" style={{fontSize:"large", border:"none"}} onClick={() => deleteItem(e)}><FaTrash/></button>
           </Li>
           ))}
